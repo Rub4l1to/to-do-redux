0822d69967c894e7445eff58c1ce27e08ad22994
Adrian Perez <adrian.perez.garcia2018@gmail.com>
2022-05-11 23:39:16 +0200

    Duplicated code

--- src/components/Iterations/styles.ts
+++ src/components/Iterations/styles.ts
@@ -16,7 +16,6 @@ export const Iterations = styled('article')<{ toggle: boolean }>`
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 
   @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
-    flex: 0 0 ${({ toggle }) => (toggle ? '400px' : '30px')};
     block-size: 100vh;
   }
 `;
